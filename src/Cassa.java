8d1626eabc5abd54b3e12342c149ee8284f21777
fraadap <daprile.francesco02@gmail.com>
2022-05-17 20:53:06 +0200

    aggiunto classe Cassa  per il metodo genera scontrino

--- src/Cassa.java
+++ src/Cassa.java
@@ -3,7 +3,22 @@ import java.util.ArrayList;
 public class Cassa {
     public Cassa(){}
 
-    public static void generaScontrino(ArrayList <Order>orders, int tavolo){
-        Array
+    //filtra la lista di ordinazioni e prende solo quelle di un certo tavolo 
+    public static void generaScontrino(ArrayList <Order>allOrders, int table){
+        ArrayList <Order> ordersTable= new ArrayList<Order>();
+        for(int i=0; i<allOrders.size(); i++){
+            if( allOrders.get(i).getTable() == table ){
+                ordersTable.add(allOrders.get(i));
+            }
+        } 
+
+        double sum = 0;
+        
+        //calcola il totale del costo dei piatti
+        for(int i=0; i<allOrders.size(); i++){
+            sum+=allOrders.get(i).getDishPrice();
+        }
+
+
     }
 }
