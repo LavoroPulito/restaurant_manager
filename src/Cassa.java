54b902ed71c0611be4454bba0a177786877204ce
fraadap <daprile.francesco02@gmail.com>
2022-05-18 17:38:49 +0200

    generaScontrino modifyed

--- src/Cassa.java
+++ src/Cassa.java
@@ -4,14 +4,7 @@ public class Cassa {
     public Cassa(){}
 
     //filtra la lista di ordinazioni e prende solo quelle di un certo tavolo 
-    public static void generaScontrino(ArrayList <Order>allOrders, int table){
-        ArrayList <Order> ordersTable= new ArrayList<Order>();
-        for(int i=0; i<allOrders.size(); i++){
-            if( allOrders.get(i).getTable() == table ){
-                ordersTable.add(allOrders.get(i));
-            }
-        } 
-
+    public static void generaScontrino(ArrayList<Order> allOrders){ 
         double sum = 0;
         
         //calcola il totale del costo dei piatti
