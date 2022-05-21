2d8e4f13cd08871c98b4cec8914eb2cdf825bac1
Near047 <niccolodisanto@gmail.com>
2022-05-21 12:42:21 +0200

    primoagg_scontrino

---
+++ src/Scontrino.java
@@ -0,0 +1,36 @@
+import com.google.gson.Gson;
+
+public class Scontrino {
+    String preconto = "";
+    boolean pagato;
+    int numeroTavoli;
+
+    public  Scontrino(){
+
+        
+        
+
+    }
+
+    public boolean pagato()
+    {
+
+        if (pagato == true)
+        {
+            return true ;
+            //facciamo in modo che la classe OrderRegister ripuliusca il tavolo
+        }
+        else
+        {
+            System.out.println("pagamento non riuscito");
+         return false ;
+        }
+    }
+
+
+}
+/*
+prendo dalla lista con un for, prendo il nome dello piatto, e prendo il costo
+a capo, e poi 
+
+*/
