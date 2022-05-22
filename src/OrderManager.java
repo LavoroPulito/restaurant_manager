82fec1b694c36ce4cdeb4557f60f0e3e992aa8c1
fraadap <daprile.francesco02@gmail.com>
2022-05-22 17:20:16 +0200

    OrderManager

---
+++ src/OrderManager.java
@@ -0,0 +1,84 @@
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+/*
+Questa classe si occupa della gestione degli ordini
+inserisce un ArrayList
+ */
+public class OrderManager {
+    
+    private HashMap<Integer, ArrayList<Order>> register;
+
+    public  OrderManager(){
+        register =  new HashMap<Integer, ArrayList<Order>>();
+    }
+    
+    public void add(ArrayList<Order> orders){ //se è una nuova ordinazione le ordinazioni vengono inserite sennò concatenate
+        int tav=orders.get(0).getTable();
+        if (register.containsKey(tav)) {
+            register.get(tav).addAll(orders);
+        } else {
+            register.put(tav, orders);
+        }
+    }
+
+    public HashMap<Integer, ArrayList<Order>> getRegister() {
+        return register;
+    }
+
+    public void setRegister(HashMap<Integer, ArrayList<Order>> register) {
+        this.register = register;
+    }
+
+    public String toString(){
+        return this.toJson();
+    }
+
+    public String toJson() { 
+        Gson gson = new Gson();
+        return gson.toJson(this, OrderManager.class);
+    }
+
+    public void cleanTable(int table){
+        if( register.containsKey(table)){
+            register.remove(table);
+        }
+    }
+
+    public void save(){
+        try {
+            File f = new File("orders.json");
+            FileWriter w = new FileWriter(f);
+            BufferedWriter writer = new BufferedWriter(w);
+            writer.write(this.toJson());
+            writer.close();
+            w.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void load(){
+        String string="";
+        String sCurrentLine="";
+        try {
+            File f = new File("orders.json");
+            FileReader r = new FileReader(f);
+            BufferedReader br = new BufferedReader(r);
+            while ((sCurrentLine = br.readLine()) != null)
+            {
+            string+=sCurrentLine;
+            }
+            br.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        Gson gson= new Gson();
+        register = gson.fromJson(string, OrderManager.class).getRegister();
+    }
+
+}
---
+++ src/OrderManagerDemo.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class OrderManagerDemo {
+
+
+    public static void main(String[] args) {
+        // creo dei dish per fare gli ordini
+        Dish dish1 = new Dish("pasta con tonno", 8.0, "pasta con tonno", "Primo");
+        Dish dish2 = new Dish("pasta al pesto", 10.0, "pasta con basilico e parmigiano", "Primo");
+        Dish dish3 = new Dish("sorbetto al limone", 9.0, "tipo gelato", "Dolce");
+        Dish dish4 = new Dish("calamari", 23.4, "calamari fritti", "Secondo");
+        Dish dish5 = new Dish("patate", 13, "patate lesse", "Secondo");
+        Dish dish6 = new Dish("pasta al sugo", 10, "pasta col pomodoro", "Primo");
+        Dish dish7 = new Dish("tiramisù", 15, "mascarpone e caffè", "Dolce");
+
+        // creo un nuovo registro
+        OrderManager orderRegister = new OrderManager();
+
+        //aggungo al registro gli ordini
+        ArrayList<Order> orders = new ArrayList<Order>();
+
+        orders.add(new Order(dish1, 1, " "));
+        orders.add(new Order(dish2, 1, " "));
+        orders.add(new Order(dish3, 2, " "));
+        orders.add(new Order(dish4, 2, " "));
+        orders.add(new Order(dish5, 2, " "));        
+        orders.add(new Order(dish6, 3, " "));
+        orders.add(new Order(dish6, 3, " "));
+        orders.add(new Order(dish7, 4, " "));
+
+        orderRegister.add(orders);
+
+        //creo la stringa json e la stampo
+        String jsonOrderRegister = orderRegister.toString();
+        System.out.println(jsonOrderRegister);
+
+        //carico il json dentro l'oggetto
+        orderRegister.load();
+
+        //salvo il json come file
+        
+        orderRegister.save();
+        
+
+        //salvo il file usando il metodo
+        orderRegister.save();
+        System.out.println("ripreso: ##################");
+        OrderManager secondoRegistro = new OrderManager();
+        secondoRegistro.load();
+        System.out.println(secondoRegistro.getRegister());
+    }
+}
