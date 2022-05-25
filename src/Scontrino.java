1f1e2771fbdac6875ca676f4026c087d1396a266
Near047 <niccolodisanto@gmail.com>
2022-05-25 22:38:44 +0200

    come giusto che sia

--- src/Scontrino.java
+++ src/Scontrino.java
@@ -7,45 +7,39 @@ public class Scontrino
 
 {
     OrderManager ordermanager = new OrderManager();
+    
 
     private HashMap<Integer, ArrayList<Order>> register;
         //tavolo:[ordini del tavolo], tavolo1:[ordini del tavolo1]
-    public Scontrino(double amount)
+    public Scontrino()
     {
-        ordermanager.load();
-        register=ordermanager.getRegister();
-        System.out.println(register);
 
-        for (int i = 0; i<register.size() ;i++ )
-        {
-
-        
-        }
-}
+    }
 
 
 
     public String preconto(int numerotavolo)
       {
           ordermanager.load();
+          register=ordermanager.getRegister();
+
           HashMap<Integer, ArrayList<Order>> reg = ordermanager.getRegister();
           System.out.print(reg);
-          /*
+          
           String preconto = "";
-          for (int i = 0; i< register.size(); i++ );
-          {
-              if (register.containsKey(numerotavolo))
-              {
-                  for (int i = 0; i< register.get(numerotavolo).size(); i++)
-                  {
-                      preconto += register.get(numerotavolo);
-                  }
-              }
-              else
-              {
+          
+        if (register.containsKey(numerotavolo))
+        {
+            for (Order ordine : register.get(numerotavolo))
+            {
+                preconto += ""
+            }
+        }
+        else
+        {
 
-              }}
-*/
+        }
+
 
 
 
