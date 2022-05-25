5e5f7dbcb18288199e68601f6d743153edee531f
Near047 <niccolodisanto@gmail.com>
2022-05-25 17:16:07 +0200

    sempre aggiornametni

--- src/Scontrino.java
+++ src/Scontrino.java
@@ -4,73 +4,30 @@ import java.util.HashMap;
 import java.io.*;
 
 public class Scontrino extends OrderManager
+
 {
-    private HashMap<Integer, ArrayList<Order>> preconto;
+    OrderManager ordermanager = new OrderManager();
+
+    private HashMap<Integer, ArrayList<Order>> register;
         //tavolo:[ordini del tavolo], tavolo1:[ordini del tavolo1]
     public Scontrino()
     {
+        ordermanager.load();
+        ordermanager.getRegister();
+        System.out.println(register);
+
+        
+
+
+
         
     }
 
     //add(ArrayList<Order> orders); SE VA IN ERRORE VA COMMENTATO
-
-
-
-
-    
-    
-    
-    
-
-        
-        public static  String getJSONFromFile (String filename) {
-            String jsonText = "";
-            try{
-            BufferedReader bufferedReader =  new BufferedReader (new FileReader (filename));
-            String line;
-            while ((line = bufferedReader. readLine() ) != null)
-            {
-            jsonText += line + "\n";
-            }
-            bufferedReader.close();
-        
-            }catch (Exception e)
-            {
-            e.printStackTrace () ;
-            }
-            System.out.println(jsonText);
-            return jsonText;
-        }
-    
+  
 }
-    
-            
+  
 
-        
-
-    
-
-        
-    
-    
-    
-
-    /*public boolean pagato()
-    {
-
-        if (pagato == true)
-        {
-            return true ;
-            //facciamo in modo che la classe OrderRegister ripuliusca il tavolo
-        }
-        else
-        {
-            System.out.println("pagamento non riuscito");
-         return false ;
-        }
-    }
-
-*/
 
 /*
 prendo dalla lista con un for, prendo il nome dello piatto, e prendo il costo
