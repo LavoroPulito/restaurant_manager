78c0383c426cbdaf080ca0e49748b53dcc0c17da
fraadap <daprile.francesco02@gmail.com>
2022-09-06 17:21:20 +0200

    TablesPanel removed

--- src/app/frontend/components/TablesPanelColored.java
+++ src/app/frontend/components/TablesPanelColored.java
@@ -1,14 +1,39 @@
 package app.frontend.components;
 
-import java.awt.*;
-import app.backend.OrderManager;
 import app.backend.Order;
+import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import app.backend.OrderManager;
 
-public class TablesPanelColored extends TablesPanel {
+public class TablesPanelColored extends JPanel implements ActionListener {
+    ArrayList<TableButton> tb;
+    ArrayList<Integer> tn;
         public TablesPanelColored(){
-        super();
-        OrderManager om = new OrderManager();
-        om.load();
+            OrderManager om = new OrderManager();
+            om.load();
+            tn=om.getTableList("preparation");
+            if (tn.size()==0){
+                JLabel l=new JLabel("Non ci sono ordinazioni da preparare al momento");
+                l.setSize(new Dimension(200,100));
+                add(l);
+            }
+            setLayout(new FlowLayout());
+            ArrayList<TableButton>buttons= new ArrayList<TableButton>();
+            for (int t:tn){
+                TableButton temp = new TableButton(t);
+                temp.addActionListener(this);
+                
+    
+                buttons.add(temp);
+                add(temp);
+            }
+            setAutoscrolls(true);
+            
+            tb=buttons;
+        
+        
         int i=0;
         for (int t:tn){
             Color color;
@@ -35,6 +60,21 @@ public class TablesPanelColored extends TablesPanel {
             i++;
             
         }
+    }
+
+    public int getNButtons(){
+        return tn.size();
+    }
+    public void addActionListener(ActionListener act){ //da passare un oggetto di una classe con metodo actionListener (Vedi app.frontend.window.CookFrame)
+        for(TableButton t: tb) {
+            t.addActionListener(act);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
