71401e64dcf163a76def57cc7d345d2e2a3622be
LavoroPulito <100956316+LavoroPulito@users.noreply.github.com>
2022-09-11 22:37:15 +0200

    add JavaDoc

---
+++ Docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"app.backend"},{"l":"app.frontend.components"},{"l":"app.frontend.windows"},{"l":"app.tests"}];updateSearchResults();
